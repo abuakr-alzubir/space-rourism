2235208619b4a12c789f4860384a0972e93cffc7
Belzzubair <107881300+abuakr-alzubir@users.noreply.github.com>
2024-09-02 05:05:15 +0300

    navigation update

--- components/navigations/Links.tsx
+++ components/navigations/Links.tsx
@@ -1,5 +1,7 @@
+"use client"
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface LinksProps {
   onClick?: () => void;
@@ -16,14 +18,10 @@ export const Links: React.FC<LinksProps> = ({
   hasLine = false,
   isTablet = false,
 }) => {
+  const pathName = usePathname();
+
   return (
-    <div
-      className={`${
-        !isvertival
-          ? "flex items-center justify-end"
-          : ""
-      }`}
-    >
+    <div className={`${!isvertival ? "flex items-center justify-end" : ""}`}>
       {hasLine && (
         <div className="w-[38%] h-1 bg-[rgba(255,255,255,0.52)] mr-[-30px] z-10"></div>
       )}
@@ -31,17 +29,28 @@ export const Links: React.FC<LinksProps> = ({
       <ul
         className={`flex  bg-[rgba(255,255,255,0.22)] w-[70%] items-center justify-center ${
           isTablet ? "py-12" : ""
-        } ${isvertival ? "flex-col gap-y-4 text-neutral-950" : "flex-row gap-x-4 h-16 text-white"}`}
+        } ${
+          isvertival
+            ? "flex-col gap-y-4 text-neutral-950"
+            : "flex-row gap-x-4 h-16 text-white"
+        }`}
       >
-        {links.map((link, i) => (
-          <li key={link.name}>
-            <Link href={link.href} onClick={onClick}>
-              {hasNubering
-                ? `0${i + 1} ${link.name.toUpperCase()}`
-                : link.name.toUpperCase()}
-            </Link>
-          </li>
-        ))}
+        {links.map((link, i) => {
+          return (
+            <li
+              key={link.name}
+              className={`hover:bg-[rgba(193,253,255,0.1)] border-white py-4 px-1 ${
+                pathName === link.href ? "border-b-4 border-white" : ""
+              }`}
+            >
+              <Link href={link.href} onClick={onClick}>
+                {hasNubering
+                  ? `0${i + 1} ${link.name.toUpperCase()}`
+                  : link.name.toUpperCase()}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
@@ -54,14 +63,14 @@ const links = [
   },
   {
     name: "destination",
-    href: "destination",
+    href: "/destination",
   },
   {
     name: "crew",
-    href: "crew",
+    href: "/crew",
   },
   {
     name: "technology",
-    href: "technology",
+    href: "/technology",
   },
 ];
